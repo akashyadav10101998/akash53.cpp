e6858f919aa9468a5d927c01adc437eb8e1b5d54
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-09-02 17:07:46 +0530

    Add files via upload

---
+++ akash53.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n1;
+cout << "wheather the number is positive, negative or zero";
+cout << "enter first number";
+cin >> n1;
+if(n1>0)
+{
+cout << "\n number " <<n1<< "is positive\n";
+}
+ else  (n1<0);
+{
+cout <<"\n number " <<n1<< "is negative\n";
+}
+ if (n1=0);
+{
+cout << "\n number \n" <<n1<< "is 0";
+}
+return 0;
+} 
+
